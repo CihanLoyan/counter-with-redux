db873070ecb322390ce24d9bf439edab9bcaa6eb
cloyan <cloyan@ebay.com>
2022-02-06 13:44:32 +0300

    mapStateToProps function added

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class Counter extends Component {
+class Counter extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <h1>{this.props.counter}</h1>
+      </div>
+    )
   }
 }
+
+function mapStateToProps(state) {
+  return { counter: state.counterReducer }
+}
+
+export default connect(mapStateToProps)(Counter)
